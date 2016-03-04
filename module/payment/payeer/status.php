eb4a8d79f4951101cb97beac1bacd845be476797
dldemoks <demokstar@gmail.com>
2016-03-04 16:46:55 +0700

    update

--- module/payment/payeer/status.php
+++ module/payment/payeer/status.php
@@ -19,6 +19,9 @@ class Payeer_Callback
     {
 		if (isset($request['m_operation_id']) && isset($request['m_sign']))
 		{
+			$err = false;
+			$message = '';
+			
 			// запись логов
 			
 			$log_text = 
@@ -34,14 +37,15 @@ class Payeer_Callback
 				"description		" . base64_decode($request["m_desc"]) . "\n" .
 				"status				" . $request["m_status"] . "\n" .
 				"sign				" . $request["m_sign"] . "\n\n";
-				
-			if (!empty($this->_log))
+			
+			$log_file = $this->_log;
+			
+			if (!empty($log_file))
 			{
-				file_put_contents($_SERVER['DOCUMENT_ROOT'] . $this->_log, $log_text, FILE_APPEND);
+				file_put_contents($_SERVER['DOCUMENT_ROOT'] . $log_file, $log_text, FILE_APPEND);
 			}
 			
-			
-			// вычисление цифровой подписи
+			// проверка цифровой подписи и ip
 			
 			$sign_hash = strtoupper(hash('sha256', implode(":", array(
 				$request['m_operation_id'],
@@ -57,151 +61,111 @@ class Payeer_Callback
 				$this->_secretKey
 			))));
 			
-			
-			// подлинность ip адреса
-			
 			$valid_ip = true;
-			$list_ip_str = str_replace(' ', '', $this->_ipfilter);
+			$sIP = str_replace(' ', '', $this->_ipfilter);
 			
-			if (!empty($list_ip_str)) 
+			if (!empty($sIP))
 			{
-				$list_ip = explode(',', $list_ip_str);
-				$this_ip_field = explode('.', $_SERVER['REMOTE_ADDR']);
-				$list_ip_field = array();
-				$i = 0;
-				$valid_ip = false;
-				foreach ($list_ip as $ip)
+				$arrIP = explode('.', $_SERVER['REMOTE_ADDR']);
+				if (!preg_match('/(^|,)(' . $arrIP[0] . '|\*{1})(\.)' .
+				'(' . $arrIP[1] . '|\*{1})(\.)' .
+				'(' . $arrIP[2] . '|\*{1})(\.)' .
+				'(' . $arrIP[3] . '|\*{1})($|,)/', $sIP))
 				{
-					$ip_field[$i] = explode('.', $ip);
-					if ((($this_ip_field[0] ==  $ip_field[$i][0]) || ($ip_field[$i][0] == '*')) &&
-						(($this_ip_field[1] ==  $ip_field[$i][1]) || ($ip_field[$i][1] == '*')) &&
-						(($this_ip_field[2] ==  $ip_field[$i][2]) || ($ip_field[$i][2] == '*')) &&
-						(($this_ip_field[3] ==  $ip_field[$i][3]) || ($ip_field[$i][3] == '*')))
-					{
-						$valid_ip = true;
-						break;
-					}
-					$i++;
+					$valid_ip = false;
 				}
 			}
 			
-			
-			// проверка цифровой подписи и ip
-		
-			if (!($request["m_sign"] == $sign_hash && $valid_ip))
+			if (!$valid_ip)
 			{
-				if (!empty($this->_emailerr))
+				$message .= " - ip address of the server is not trusted\n" . 
+				"   trusted ip: " . $sIP . "\n" . 
+				"   ip of the current server: " . $_SERVER['REMOTE_ADDR'] . "\n";
+				$err = true;
+			}
+
+			if ($request["m_sign"] != $sign_hash)
+			{
+				$message .= " - Do not match the digital signature\n";
+				$err = true;
+			}
+			
+			if (!$err)
+			{
+				// загрузка заказа
+			
+				$oDB = AMI::getSingleton('db');
+
+				$order = $oDB->fetchRow(
+					DB_Query::getSnippet("SELECT `status`,`sysinfo`,`total` FROM `cms_es_orders` WHERE `id` = %s")
+					->q($request['m_orderid'])
+				);
+				
+				$sysinfo = unserialize($order['sysinfo']);
+				$order_curr = ($sysinfo['fee_curr'] == 'RUR') ? 'RUB' : $sysinfo['fee_curr'];
+				$order_amount = number_format($order['total'], 2, '.', '');
+				
+				// проверка суммы и валюты
+				
+				if ($request['m_amount'] != $order_amount)
 				{
-					$message = "Failed to make the payment through Payeer for the following reasons:\n\n";
-					
-					if (!$valid_ip)
+					$message .= " - Wrong amount\n";
+					$err = true;
+				}
+
+				if ($request['m_curr'] != $order_curr)
+				{
+					$message .= $this->language->get('text_email_message8') . "\n";
+					$err = true;
+				}
+				
+				// проверка статуса
+				
+				if (!$err && $order['status'] !== 'checkout')
+				{
+					switch ($request['m_status'])
 					{
-						$message .= " - ip address of the server is not trusted\n" . 
-									"   trusted ip: " . $this->_ipfilter . "\n" . 
-									"   ip of the current server: " . $_SERVER['REMOTE_ADDR'] . "\n";
+						case 'success':
+							$qupdate = $oDB->fetchValue(DB_Query::getUpdateQuery(
+								'cms_es_orders',
+								array('status'  => 'confirmed_done'),
+								DB_Query::getSnippet('WHERE id IN (%s)')->q($request['m_orderid'])
+							));
+							break;
+							
+						default:
+							if ($order['order_status_id'] !== $this->config->get('payeer_order_fail_id'))
+							{
+								$message .= " - The payment status is not success\n";
+								$qupdate = $oDB->fetchValue(DB_Query::getUpdateQuery(
+									'cms_es_orders',
+									array('status'  => 'cancelled'),
+									DB_Query::getSnippet('WHERE id IN (%s)')->q($request['m_orderid'])
+								));
+							}
+							$err = true;
+							break;
 					}
-					
-					if ($request["m_sign"] != $sign_hash)
-					{
-						$message .= " - Do not match the digital signature\n";
-					}
-					
-					$message .= "\n" . $log_text;
+				}
+			}
+			
+			if ($err)
+			{
+				$to = $this->_emailerr;
+				
+				if (!empty($to))
+				{
+					$message = "Failed to make the payment through Payeer for the following reasons:\n\n" . $message . "\n" . $log_text;
 					$headers = "From: no-reply@" . $_SERVER['HTTP_SERVER'] . "\r\n" . 
-								"Content-type: text/plain; charset=utf-8 \r\n";
-						
-					mail($this->_emailerr, 'Payment error', $message, $headers);
+					"Content-type: text/plain; charset=utf-8 \r\n";
+					mail($to, 'Payment error', $message, $headers);
 				}
 
 				return $request['m_orderid'] . '|error';
 			}
-			
-			
-			// загрузка заказа
-			
-			$oDB = AMI::getSingleton('db');
-
-			$order = $oDB->fetchRow(
-				DB_Query::getSnippet("SELECT `status`,`sysinfo`,`total` FROM `cms_es_orders` WHERE `id` = %s")
-				->q($request['m_orderid'])
-			);
-			
-			$sysinfo = unserialize($order['sysinfo']);
-			$order_curr = ($sysinfo['fee_curr'] == 'RUR') ? 'RUB' : $sysinfo['fee_curr'];
-			$order_amount = number_format($order['total'], 2, '.', '');
-			
-			
-			// проверка суммы и валюты
-			
-			if (!($request['m_amount'] == $order_amount && $request['m_curr'] == $order_curr))
+			else
 			{
-				if (!empty($this->_emailerr))
-				{
-					$message = "Failed to make the payment through Payeer for the following reasons:\n\n";
-					
-					if ($request['m_amount'] != $order_amount)
-					{
-						$message .= " - Wrong amount\n";
-					}
-					
-					if ($request['m_curr'] != $order_curr)
-					{
-						$message .= " - Wrong currency\n";
-					}
-					
-					$message .= "\n" . $log_text;
-					$headers = 
-						"From: no-reply@" . $_SERVER['HTTP_SERVER'] . "\r\n" . 
-						"Content-type: text/plain; charset=utf-8 \r\n";
-						
-					mail($this->_emailerr, 'Payment error', $message, $headers);
-				}
-
-				return $request['m_orderid'] . '|error';
-			}
-			
-			
-			// проверка статуса
-			
-			if ($order['status'] != 'checkout')
-			{
-				switch ($request['m_status'])
-				{
-					case 'success':
-						$qupdate = $oDB->fetchValue(DB_Query::getUpdateQuery(
-							'cms_es_orders',
-							array('status'  => 'confirmed_done'),
-							DB_Query::getSnippet('WHERE id IN (%s)')->q($request['m_orderid'])
-						));
-						
-						return $request['m_orderid'] . '|success';
-						break;
-						
-					case 'fail':
-						$qupdate = $oDB->fetchValue(DB_Query::getUpdateQuery(
-							'cms_es_orders',
-							array('status'  => 'cancelled'),
-							DB_Query::getSnippet('WHERE id IN (%s)')->q($request['m_orderid'])
-						));
-						
-						if (!empty($this->_emailerr))
-						{
-							$to = $this->_emailerr;
-							$subject = "Payment error";
-							$message = "Failed to make the payment through Payeer for the following reasons:\n\n";
-							$message .= " - The payment status is not success\n";
-							$message .= "\n" . $log_text;
-							$headers = "From: no-reply@" . $_SERVER['HTTP_SERVER'] . "\r\nContent-type: text/plain; charset=utf-8 \r\n";
-							mail($to, $subject, $message, $headers);
-						}
-					
-						return $request['m_orderid'] . '|error';
-						break;
-						
-					default: 
-						return $request['m_orderid'] . '|error';
-						break;
-				}
+				return $request['m_orderid'] . '|success';
 			}
 		}
 		else
