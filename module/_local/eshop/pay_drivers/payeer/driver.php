e8f50aafccfbb418cbe7210c2c9648cab9fefecd
dldemoks <demokstar@gmail.com>
2021-09-23 15:58:31 +0000

    update security

--- module/_local/eshop/pay_drivers/payeer/driver.php
+++ module/_local/eshop/pay_drivers/payeer/driver.php
@@ -1,17 +1,16 @@
 <?php
 
-class Payeer_PaymentSystemDriver extends AMI_PaymentSystemDriver
-{
-    protected $driverName = 'payeer';
+class Payeer_PaymentSystemDriver extends AMI_PaymentSystemDriver {
+  
+  protected $driverName = 'payeer';
 
-    public function getPayButton(&$aRes, $aData, $bAutoRedirect = false)
-    {
-        foreach (array('return', 'description') as $k)
-		{
-            $aData[$fldName] = htmlspecialchars($aData[$k]);
-        }
+  public function getPayButton(&$aRes, $aData, $bAutoRedirect = false) {
+    
+    foreach (array('return', 'description') as $k) {
+      $aData[$fldName] = htmlspecialchars($aData[$k]);
+    }
 
-        $aEclusion = array(
+    $aEclusion = array(
 			'return',
 			'cancel',
 			'callback',
@@ -23,35 +22,27 @@ class Payeer_PaymentSystemDriver extends AMI_PaymentSystemDriver
 			'order',
 			'button_name',
 			'button'
-        );
+    );
 
-        $hiddens = '';
-        foreach ($aData as $key => $value) 
-		{
-            if (!in_array($key, $aEclusion)) 
-			{
-                $hiddens .=
-                    '<input type="hidden" name="' . $key .
-                    '" value="' . (is_null($value) ? $aData[$key] : $value) .
-                    '" />' . "\n";
-            }
-        }
-        $aData['hiddens'] = $hiddens;
-
-        return parent::getPayButton($aRes, $aData, $bAutoRedirect);
+    $hiddens = '';
+    foreach ($aData as $key => $value) {
+      if (!in_array($key, $aEclusion)) {
+        $hiddens .= '<input type="hidden" name="' . $key . '" value="' . (is_null($value) ? $aData[$key] : $value) . '" />\n';
+      }
     }
+    
+    $aData['hiddens'] = $hiddens;
+    return parent::getPayButton($aRes, $aData, $bAutoRedirect);
+  }
 
-    public function getPayButtonParams($aData, &$aRes)
-    {
-        $aData += array('payeer_payment' => 0);
+  public function getPayButtonParams($aData, &$aRes) {
+    $aData += array('payeer_payment' => 0);
 		
-		if ($aData['currency'] == 'RUR')
-		{
+		if ($aData['currency'] == 'RUR') {
 			$aData['currency'] = 'RUB';
 		}
 		
 		$aData['description'] = base64_encode($aData['description']);
-
 		$m_shop 	= $aData['payeer_shop'];
 		$m_orderid 	= $aData['order'];
 		$m_amount 	= number_format($aData['amount'], 2, '.', '');
@@ -68,22 +59,18 @@ class Payeer_PaymentSystemDriver extends AMI_PaymentSystemDriver
 			$m_key
 		);
 		$aData['sign'] = strtoupper(hash('sha256', implode(":", $arHash)));
-		
-        return parent::getPayButtonParams($aData, $aRes);
-    }
+    return parent::getPayButtonParams($aData, $aRes);
+  }
 
-    public function payProcess($aGet, $aPost, &$aRes, $aCheckData, $aOrderData)
-    {
-        return parent::payProcess($aGet, $aPost, $aRes, $aCheckData, $aOrderData);
-    }
+  public function payProcess($aGet, $aPost, &$aRes, $aCheckData, $aOrderData) {
+    return parent::payProcess($aGet, $aPost, $aRes, $aCheckData, $aOrderData);
+  }
 
-    public function payCallback($aGet, $aPost, &$aRes, $aCheckData, $aOrderData)
-    {
-        return 1;
-    }
+  public function payCallback($aGet, $aPost, &$aRes, $aCheckData, $aOrderData) {
+    return 1;
+  }
 
-    public function getProcessOrder($aGet, $aPost, &$aRes, $aAdditionalParams)
-    {
-        return parent::getProcessOrder($aGet, $aPost, $aRes, $aAdditionalParams);
-    }
+  public function getProcessOrder($aGet, $aPost, &$aRes, $aAdditionalParams) {
+    return parent::getProcessOrder($aGet, $aPost, $aRes, $aAdditionalParams);
+  }
 }
